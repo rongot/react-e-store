308f4521cf60047c78ed852db207b4dd03f4b7e0
rongot <rgotliv@gmail.com>
2023-09-05 12:58:56 +0300

    update style

--- e-store/src/App.js
+++ e-store/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[results,setResults]=useState([])
+
+React.useEffect(()=>{
+  fetch("http://localhost:5000/categories")
+  .then(res=>res.json())
+  .then(data=>{
+    console.log(data)
+    setResults(data)
+  })
+  
+},[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        results.map(d=>(
+          
+          <div key={d.id}>{d.title}
+            
+          </div>
+        ))
+      }
     </div>
   );
 }
